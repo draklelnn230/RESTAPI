9607b33b685ba1bc0e535c0a42ef7a397e637826
Lan <h.baolan20020@gmail.com>
2022-02-10 16:23:15 +0700

    Understanding of Basic RestAPIs

---
+++ controllers/feed.js
@@ -0,0 +1,19 @@
+exports.getPosts = (req, res, next) => {
+    res.json({ 
+        posts: [{ title: 'First Post', content: 'This is the first post!' }]
+    });
+};
+
+exports.createPost = (req, res, next) => {
+
+    const title = req.body.title;
+    const content = req.body.content
+
+    console.log(title, content);
+
+    // Create post in db
+    res.status(201).json({
+        message: 'Post created successfully',
+        post: {id: new Date(), title: title, content: content}
+    })
+}
