3ea96f940b7226d9fa6a40e3da46007465260bc5
Lan <h.baolan20020@gmail.com>
2022-02-12 11:10:19 +0700

    Working with Rest APIs, get some errors with file upload

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
+const path = require('path')
 const express = require('express');
 const bodyParse = require('body-parser');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
+
 
 const feedRoutes = require('./routes/feed')
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+      cb(null, uuidv4() + '-' + file.originalname);
+    }
+  });
+  
+  const fileFilter = (req, file, cb) => {
+    if (
+      file.mimetype === 'image/png' ||
+      file.mimetype === 'image/jpg' ||
+      file.mimetype === 'image/jpeg'
+    ) {
+      cb(null, true);
+    } else {
+      cb(null, false);
+    }
+  };
+
 app.use(bodyParse.json()); // application / json
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'));
+app.use('/images', express.static(path.join(__dirname, 'images')))
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -16,5 +44,18 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes)
 
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({message: message});
+})
 
-app.listen(8080);
+mongoose
+    .connect(
+        'mongodb+srv://drakeln230:JelIvr3cgKCm3SZF@cluster0.ywr61.mongodb.net/messages?retryWrites=true&w=majority'
+    )
+    .then(result => {
+        app.listen(8080);
+    })
+    .catch(err => console.log(err))
